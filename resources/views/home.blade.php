48b57a39e43475f309c40b61aa86227c93257729
yueguang <yueguang@qingmutec.com>
2017-12-15 14:57:58 +0800

    生成侧栏目页面

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,18 +3,28 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+        <div class="col-lg-12">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading">Zy管理系统</div>
 
                 <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                    <div class="col-lg-3">
+                @include('layouts.left')
+                    </div>
+                    <div class="col-lg-9">
+                        @if (session('status'))
+                            <div class="alert alert-success">
+                                {{ session('status') }}
+                            </div>
+                        @endif
 
-                    You are logged in!
+                            <h1>欢迎登陆系统！</h1>
+                            <p>这是个人git项目。</p>
+                            <p><a class="btn btn-primary btn-lg" role="button">
+                                    了解更多</a>
+                            </p>
+
+                    </div>
                 </div>
             </div>
         </div>
---
+++ resources/views/layouts/left.blade.php
@@ -0,0 +1,65 @@
+<div class="panel panel-default">
+    <div class="panel-heading">
+        <h3 class="panel-title">
+            权限管理
+        </h3>
+    </div>
+    <div>
+        <div class="panel-body">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h4 class="panel-title">
+                        <a data-toggle="collapse" data-parent="#accordion"
+                           href="#collapseOne">
+                            用户管理
+                        </a>
+                    </h4>
+                </div>
+                <div id="collapseOne" class="panel-collapse collapse">
+                    <div class="panel-body">
+                        <ul class="nav nav-pills nav-stacked">
+                            <li><a href="#">用户列表</a></li>
+                            <li><a href="#">用户添加</a></li>
+                            <li><a href="#">用户BI</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="panel-heading">
+                    <h4 class="panel-title">
+                        <a data-toggle="collapse" data-parent="#accordion"
+                           href="#collapseTwo">
+                            角色管理
+                        </a>
+                    </h4>
+                </div>
+                <div id="collapseTwo" class="panel-collapse collapse">
+                    <div class="panel-body">
+                        <ul class="nav nav-pills nav-stacked">
+                            <li><a href="#"> 角色列表</a></li>
+                            <li><a href="#">角色添加</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="panel-heading">
+                    <h4 class="panel-title">
+                        <a data-toggle="collapse" data-parent="#accordion" class=""
+                           href="#collapseThr">
+                            权限管理
+                        </a>
+                    </h4>
+                </div>
+                <div id="collapseThr" class="panel-collapse collapse">
+                    <div class="panel-body">
+                        <ul class="nav nav-pills nav-stacked">
+                            <li><a href="#"> 权限列表</a></li>
+                            <li><a href="#">权限添加</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
